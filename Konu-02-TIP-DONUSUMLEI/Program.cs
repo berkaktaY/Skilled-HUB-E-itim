44ef49942ae9b0aeb2e2bc9f5af5c0bfca6062bb
berkaktaY <124641330+berkaktaY@users.noreply.github.com>
2025-05-28 01:27:14 +0300

    Konu-3-Operatörler'e geçiş

--- Konu-02-TIP-DONUSUMLEI/Program.cs
+++ Konu-02-TIP-DONUSUMLEI/Program.cs
@@ -36,6 +36,55 @@
             Console.WriteLine("Kesirli Sayı Değeri: " + sayi2);
             Console.WriteLine("Tam Sayı Değeri:" + TamSayi);
             Console.WriteLine();
+
+
+
+            // --------- Diğer Tür Değişim Yöntemleri ----------
+
+            Console.WriteLine("Diğer Tür Dönüşümleri");
+
+            int TamSayı = 10;
+
+            double KesirliSayı3= 5.25;
+
+            bool İslemSonuc = true;
+
+            Console.WriteLine("Lütfen Bir Sayı Giriniz: ");
+
+            var GirilenDeğer = Console.ReadLine(); // var ' ı kullanıcının gireceği değer belirsiz olduğu için var sayesinde sistem otomatik türünü belirler
+
+            Console.WriteLine("Girilen değerin türü: " + GirilenDeğer.GetType());
+
+            var parsSayı = int.Parse(GirilenDeğer); // Girilen değeri string türünden , int türüne dönüştürür
+
+            Console.WriteLine("int.parse + Tamsayı2: " + (int.Parse(GirilenDeğer)) + TamSayi);
+            Console.WriteLine("Double.parse + Tamsayı2: " + (double.Parse(GirilenDeğer)) + TamSayi);
+
+            Console.WriteLine("Decimal.parse + Tamsayı2: " + (decimal.Parse(GirilenDeğer)) + TamSayi);
+
+
+            //-------------- Convert Tür Dönüşümü --------------
+
+            Console.WriteLine("Convert Tür Dönüşümü");
+            Console.WriteLine(Convert.ToString(TamSayı).GetType());
+            Console.WriteLine(Convert.ToDouble(TamSayı).GetType());
+            Console.WriteLine(Convert.ToInt32(KesirliSayı3).GetType());
+            Console.WriteLine(Convert.ToString(İslemSonuc).GetType());
+
+            //---------------- object -----------------
+
+            object nesne = "10153416"; // object türü her türlü veriyi tutabilir
+
+            Console.WriteLine("Nesnenin veri tipi: " + nesne.GetType());
+
+            nesne = Convert.ToDecimal(nesne); // nesne artık decimal türünde
+
+            Console.WriteLine("Nesnenin türü: " + nesne.GetType());
+            Console.WriteLine("Nesne değeri: " + nesne);
+            
+
+
         }
+
     }
 }
---
+++ Konu-3-OERATÖRLER/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Konu_3_OERATÖRLER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //---------- 1-ARİTMETİK OPERATÖRLER ----------
+
+            int Sayı1= 10;
+            int Sayı2 = 5;
+            int SAyı3 = 2;
+
+        }
+    }
+}
